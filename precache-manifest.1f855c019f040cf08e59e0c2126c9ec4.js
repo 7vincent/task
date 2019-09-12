9d03ea10b1977449228bf2af07a1eb4afba087ca
Vicente Albuquerque <7.vincent@gmail.com>
2019-09-12 00:39:43 -0300

    Updates

---
+++ precache-manifest.1f855c019f040cf08e59e0c2126c9ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a655c8c2dcfbd289d63793233c01c832",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "ddbaa882aca46005d7b2",
+    "url": "/task/static/css/main.e75520f6.chunk.css"
+  },
+  {
+    "revision": "f26e5265258cc2a32e6e",
+    "url": "/task/static/js/2.00f01475.chunk.js"
+  },
+  {
+    "revision": "ddbaa882aca46005d7b2",
+    "url": "/task/static/js/main.3dfb0c93.chunk.js"
+  },
+  {
+    "revision": "e97626bb8fc3f21895c9",
+    "url": "/task/static/js/runtime~main.6305ab2c.js"
+  },
+  {
+    "revision": "0b0a90e92834bcc7a698b01a0844c5f6",
+    "url": "/task/static/media/perfil.0b0a90e9.png"
+  }
+]);
